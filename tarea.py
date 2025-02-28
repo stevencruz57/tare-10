293524f67efb98ef0afd565ff45c83d89dbcacc0
stevencruz57 <sa.cruzs@uea.edu.ec>
2025-02-28 00:31:21 -0500

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,13 @@
+# primero creamos una matriz bidimensional 3*3
+matriz = [
+   [9,3,6],
+   [3,4,2],
+   [8,9,3]
+]
+# Imprimimos está matriz
+print(matriz)
+# luego seleccionamos la fila que deseamos y la ordenamos con sort
+fila1=matriz[0]
+fila1.sort()
+# para finalizar imprimimos nuevamente la matriz está es con la fila ordenada
+print(matriz)
